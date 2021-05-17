7f87eefc2b7c537fff2b2010b1aa796e66f351c7
mabel28 <mabel28.46@gmail.com>
2021-05-17 16:38:50 -0300

    added custom user model.

---
+++ app/app/calc.py
@@ -0,0 +1,16 @@
+def add(x, z):
+    """Add two number together"""
+    return x + z
+
+
+def subtract(x, z):
+    """Subtract x from y and return value"""
+    return x - z
+
+
+def algo(x, c):
+    if x > c:
+        x = x * 2
+    if x > 4:
+        x = 0
+    return x
